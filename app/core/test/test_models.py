76a9df7fe1d532730a2ab2c43afceaa6dbfc3e3b
Ankit Shukla <ankits2912@gmail.com>
2019-10-10 17:15:52 +0530

    add custome superuser admin

---
+++ app/core/test/test_models.py
@@ -0,0 +1,35 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_success(self):
+        """test creating user with an email is successful"""
+        email="nitin@gmail.com"
+        password="nitin123"
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+        self.assertEqual(user.email,email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email(self):
+        email = 'nitin@GMAIL.com'
+        user = get_user_model().objects.create_user(email, 'nitin123')
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """test creating new user email validate"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'nitin123')
+
+    def test_create_new_superuser(self):
+        """Test create new super user"""
+        user = get_user_model().objects.create_superuser(
+            'nitin@gmail.com',
+            'nitin123'
+        )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
